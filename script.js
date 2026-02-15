b8b0b52c6fde9b09038b4fe4b4eebc3bbce58d58
Asavari Charati <asavaricharati@gmail.com>
2026-02-15 15:35:02 +0530

    completed the bag section and removed the eye thing it was kinda weird

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const eyes = document.querySelectorAll(".eye");
+const bagSection = document.querySelector(".bag-section");
 
-  let targetX = 0;
-  let targetY = 0;
-  let currentX = 0;
-  let currentY = 0;
+window.addEventListener("scroll", () => {
+  const rect = bagSection.getBoundingClientRect();
+  const triggerPoint = window.innerHeight * 0.65;
 
-  document.addEventListener("mousemove", (e) => {
-    targetX = (e.clientX / window.innerWidth - 0.5) * 12;
-    targetY = (e.clientY / window.innerHeight - 0.5) * 3;
-  });
-
-  function animateEyes() {
-    currentX += (targetX - currentX) * 0.15;
-    currentY += (targetY - currentY) * 0.15;
-
-    eyes.forEach((eye) => {
-      eye.style.transform = `translate(${currentX}px, ${currentY}px)`;
-    });
-
-    requestAnimationFrame(animateEyes);
+  if(rect.top < triggerPoint && rect.bottom > 100){
+    bagSection.classList.add("active");
+  } else {
+    bagSection.classList.remove("active");
   }
-
-  animateEyes();
 });
